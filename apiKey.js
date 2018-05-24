db1a0611f192f1ab7fb49e425df70529844a98e2
Chaitanya <chaitanya9816@gmail.com>
2018-05-24 16:46:07 +0530

    message

---
+++ apiKey.js
@@ -0,0 +1,2 @@
+const API_KEY = '21fc92cb';
+module.exports = API_KEY;
---
+++ requireDependencies.js
@@ -0,0 +1,5 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const API_KEY = require('./apiKey');
